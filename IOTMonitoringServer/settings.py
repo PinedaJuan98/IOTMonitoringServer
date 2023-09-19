d5298eb76c1895b7bc93f8211b2a0e0670b138e5
PinedaJuan98 <juan.pineda@uniandes.edu.co>
2023-09-19 17:41:27 -0500

    Updates

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -163,11 +163,11 @@ MQTT_PORT = 8082
 
 # Credenciales para el usuario suscriptor del bróker MQTT
 MQTT_USER = "admin"
-MQTT_PASSWORD = "admin"
+MQTT_PASSWORD = "Sociales98"
 
 # Credenciales para el usuario publicador del bróker MQTT
 MQTT_USER_PUB = "admin2"
-MQTT_PASSWORD_PUB = "admin2"
+MQTT_PASSWORD_PUB = "Sociales99"
 
 # Tópico a suscribir. "+/+/+/+/out" se suscribe únicamente a los
 # tópicos con forma <país>/<estado>/<ciudad>/<usuario>/out.
